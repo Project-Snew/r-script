5c868eb0d5713ed8f386abcad3c319a088898f94
Shashank Srivastava <shashanksri65@gmail.com>
2020-07-20 21:59:37 +0530

    Got all comments and replies with parentid and commentid

--- title.py
+++ title.py
@@ -8,13 +8,21 @@ reddit = praw.Reddit(client_id = 'N_9TUKaeGtQ_1A',
 
 subreddit = reddit.subreddit('tits')
 
-hot_title = subreddit.hot(limit=2)
+hot_title = subreddit.hot(limit=3)
 
 for submission in hot_title:
     if not submission.stickied:
         print(submission.title)
 
-        comments = submission.comments.list()
-        for comment in comments:
+        submission.comments.replace_more(limit=0)
+
+        for comment in submission.comments.list():
             print(20 * '--')
+            print('Parent ID:', comment.parent())
+            print('Comment ID:, comment.id')
             print(comment.body)
+
+
+#Now we have to make program to just check the parent id and create a structure to make all
+#comments and replies in proper structure. We'll just have all those in same line which 
+#have the same parent id. That's easy.
